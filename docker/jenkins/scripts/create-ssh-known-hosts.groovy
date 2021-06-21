fa0ef608c04ad493c763364a6502806489acc418
Erik Stedje <estedje@users.noreply.github.com>
2021-06-21 21:40:47 +0200

    wip

--- docker/jenkins/scripts/create-ssh-known-hosts.groovy
+++ docker/jenkins/scripts/create-ssh-known-hosts.groovy
@@ -4,7 +4,7 @@ import org.yaml.snakeyaml.Yaml
 File configFile = this.args[0] as File
 Map config = new Yaml().load(configFile.text)
 List<String> knownHosts = config.ssh.knownHosts
-println "Creating /etc/ssh/ssh_known_hosts..."
-File outputFile = new File('/etc/ssh/ssh_known_hosts')
+println "Creating ~/.ssh/known_hosts..."
+File outputFile = new File('~/.ssh/known_hosts')
 outputFile.delete()
 outputFile << knownHosts.join('\n')
